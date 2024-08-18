e20a0b0361889d3e2efef0a868df3a8075915bd5
abdulismailca <abdulismailca@gmail.com>
2024-08-18 08:10:19 +0530

    Update display_data.php

--- display_data.php
+++ display_data.php
@@ -22,10 +22,12 @@ $result = $conn->query($sql);
 <html>
 <head>
     
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/test.css">
 </head>
-<body>
+<body class="bdyd">
 <div id="navbar">
     <div id="left">
         <img src="assets/images/bpcllogo.jpeg" alt="logo" />
