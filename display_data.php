99cad3d7667f541eba9c185e0e7ebfadef011910
abdulismailca <abdulismailca@gmail.com>
2024-08-18 08:21:45 +0530

    Update display_data.php

--- display_data.php
+++ display_data.php
@@ -25,7 +25,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/test.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body class="bdyd">
 <div id="navbar">
